1139a6365df835b13a1b6d1a0f0302ab889b01f1
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-11-08 17:12:04 -0300

    valores de retorno - return

--- funcoes.py
+++ funcoes.py
@@ -33,3 +33,36 @@ def describe_pet(pet_name, pet_type='Dog'):
 describe_pet('Kalinda')
 describe_pet(pet_type='cat', pet_name='Dora')
 
+
+# Valores de retorno (return)
+
+def formatted_name(first_name, last_name):
+    """Devolve um nome completo formatado de modo elegante."""
+    full_name = f'{first_name} {last_name}'
+    return full_name.title()
+
+
+nome = formatted_name('hugo', 'gusmão')
+print(nome)
+
+musician = formatted_name('jimi', 'hendrix')
+print(musician)
+
+#Deixando um argumento opcional - nome do meio
+
+
+def formatted_name(first_name, last_name, middle_name=''):
+    if middle_name:
+        full_name = f'{first_name} {middle_name} {last_name}'
+        return full_name.title()
+    else:
+        full_name = f'{first_name} {last_name}'
+        return full_name.title()
+
+
+nome_simples = formatted_name('jimi', 'hendrix')
+nome_composto = formatted_name('john', 'hooker', 'lee')
+
+
+print(nome_simples)
+print(nome_composto)
