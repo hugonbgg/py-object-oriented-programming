64daadedffebd0b93e1a3a59d1ea63d1b82c45ad
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-11-09 12:13:33 -0300

    Exercicios sobre dict e input

--- exercicios_funcoes.py
+++ exercicios_funcoes.py
@@ -65,6 +65,68 @@ describe_city('Boston', 'Estados Unidos')
 describe_city('Paris', 'França')
 describe_city('Tokyo', 'Japão')
 
+"""8.6 – Nomes de cidade: Escreva uma função chamada city_country() que
+aceite o nome de uma cidade e seu país. A função deve devolver uma string
+formatada assim:
+"Santiago, Chile"
+Chame sua função com pelo menos três pares cidade-país e apresente o valor
+devolvido."""
 
 
+def city_country():
+    city = input('Digite a cidade:')
+    country = input('Digite o país:')
+    print(f'{city.title()}, {country.title()}.')
 
+
+city_country()
+
+"""8.7 – Álbum: Escreva uma função chamada make_album() que construa um
+dicionário descrevendo um álbum musical. A função deve aceitar o nome de um
+artista e o título de um álbum e deve devolver um dicionário contendo essas duas
+informações. Use a função para criar três dicionários que representem álbuns
+diferentes. Apresente cada valor devolvido para mostrar que os dicionários estão
+armazenando as informações do álbum corretamente.
+Acrescente um parâmetro opcional em make_album() que permita armazenar o
+número de faixas em um álbum. Se a linha que fizer a chamada incluir um valor
+para o número de faixas, acrescente esse valor ao dicionário do álbum. Faça pelo
+menos uma nova chamada da função incluindo o número de faixas em um álbum."""
+
+
+def make_album(artist, album, faixas=''):
+    artist_album = {'artista': artist, 'álbum': album}
+    if faixas:
+        artist_album['faixas'] = faixas
+    print(artist_album)
+
+
+make_album('U2', 'Live in Paris')
+make_album('U2', 'Live in Paris', '12')
+
+"""8.8 – Álbuns dos usuários: Comece com o seu programa do Exercício 8.7.
+Escreva um laço while que permita aos usuários fornecer o nome de um artista e o
+título de um álbum. Depois que tiver essas informações, chame make_album() com
+as entradas do usuário e apresente o dicionário criado. Lembre-se de incluir um
+valor de saída no laço while."""
+
+
+def make_album():
+    coletanea = []
+    while True:
+        print("Pressione 'q' para sair.")
+        artist = input('Digite o nome do artista:')
+        if artist == 'q':
+            print("'q' key pressed! Exiting...")
+            break
+        album = input('Digite o nome do álbum:')
+        if album == 'q':
+            print("'q' key pressed! Exiting...")
+            break
+        artist_album = {'artista': artist, 'álbum': album}
+        print(artist_album)
+        coletanea.append(artist_album)
+
+    print(f'Os artistas digitados foram:{coletanea}')
+
+
+make_album()
