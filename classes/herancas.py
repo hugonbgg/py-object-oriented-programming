6f2555e5a198c845ec67d9bb7d76894be240df78
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-11-21 17:46:21 -0300

    introdução a heranças

---
+++ classes/herancas.py
@@ -0,0 +1,51 @@
+class Car():
+    """Uma tentativa simples de representar um carro."""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+carro = Car('Honda', 'Acura', 2019)
+carro.get_descriptive_name()
+carro.read_odometer()
+carro.update_odometer(23)
+carro.read_odometer()
+carro.increment_odometer(100)
+carro.read_odometer()
+
+
+class EletricCar(Car):
+    """Presenta aspectos especificos de veiculos elétricos"""
+    def __init__(self, make, model, year):
+        """Inicializa os atributos da classe-pai"""
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """Exibe uma frase que descreve a capacidade da bateria"""
+        print(f"This car has a {self.battery_size} kwh battery.")
+
+
+carro_eletrico = EletricCar('Tesla', 'Model S', 2019)
+print(carro_eletrico.get_descriptive_name())
+carro_eletrico.describe_battery()
+carro_eletrico.battery_size = 100
+carro_eletrico.describe_battery()
