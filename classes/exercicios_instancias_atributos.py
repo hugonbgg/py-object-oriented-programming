94975f37ec5c30e04b5387422beb52637b53468c
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-11-23 18:11:44 -0300

    exercicios

---
+++ classes/exercicios_instancias_atributos.py
@@ -0,0 +1,31 @@
+"""9.6 – Sorveteria: Uma sorveteria é um tipo específico de restaurante. Escreva uma
+classe chamada IceCreamStand que herde da classe Restaurant escrita no
+Exercício 9.1 (página 225) ou no Exercício 9.4 (página 232). Qualquer versão da
+classe funcionará; basta escolher aquela de que você mais gosta. Adicione um
+atributo chamado flavors que armazene uma lista de sabores de sorvete. Escreva
+um método para mostrar esses sabores. Crie uma instância de IceCreamStand e
+chame esse método."""
+
+
+
+"""9.7 – Admin: Um administrador é um tipo especial de usuário. Escreva uma classe
+chamada Admin que herde da classe User escrita no Exercício 9.3 (página 226),
+ou no Exercício 9.5 (página 232). Adicione um atributo privileges que armazene
+uma lista de strings como "can add post", "can delete post" "can ban user",
+e assim por diante. Escreva um método chamado show_privileges() que liste o
+conjunto de privilégios de um administrador. Crie uma instância de Admin e chame
+seu método."""
+
+"""9.8 – Privilégios: Escreva uma classe Privileges separada. A classe deve ter um
+atributo privileges que armazene uma lista de strings conforme descrita no
+Exercício 9.7. Transfira o método show_privileges() para essa classe. Crie uma
+instância de Privileges como um atributo da classe Admin. Crie uma nova
+instância de Admin e use seu método para exibir os privilégios."""
+
+"""9.9 – Upgrade de bateria: Use a última versão de electric_car.py desta seção.
+Acrescente um método chamado upgrade_battery() na classe Battery. Esse
+método deve verificar a capacidade da bateria e defini-la com 85 se o valor for
+diferente. Crie um carro elétrico com uma capacidade de bateria default, chame
+get_range() uma vez e, em seguida, chame get_range() uma segunda vez após
+fazer um upgrade da bateria. Você deverá ver um aumento na distância que o
+carro é capaz de percorrer."""
